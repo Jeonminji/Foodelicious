74f89e75f4717c93cf63f6fc0df99f172688aa72
전민지 <53859836+Jeonminji@users.noreply.github.com>
2021-01-21 12:55:55 +0900

    Create s_double_check.java

---
+++ java/Seller/s_double_check.java
@@ -0,0 +1,28 @@
+package com.example.fd1.Seller;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class s_double_check extends StringRequest {
+
+    //서버 url 설정(php파일 연동)
+    final static private String URL = "http://foodelicious.dothome.co.kr/s_double_check.php";
+    private Map<String, String> map;
+
+    public s_double_check(String c_id, Response.Listener<String>listener){
+        super(Request.Method.POST,URL,listener,null);
+
+        map = new HashMap<>();
+        map.put("c_id",c_id);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
